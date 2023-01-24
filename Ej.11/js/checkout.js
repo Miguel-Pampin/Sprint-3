c3315a046d52a6071f3192ea42bf48d161ab2f7d
Miguel <miguelpampiniglesias@gmail.com>
2023-01-24 10:46:19 +0100

    Revision

--- Ej.11/js/checkout.js
+++ Ej.11/js/checkout.js
@@ -1,6 +1,6 @@
 
 // Exercise 6
-function validate() {
+function validate(event) {
 	
 	let error = 0;
 	// Get the input fields
@@ -12,12 +12,19 @@ function validate() {
 	let fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
-	let errorName = document.getElementById("errorName");
+	/* let errorName = document.getElementById("errorName");
 	let errorLastN = document.getElementById("errorLastN");
 	let errorEmail = document.getElementById("errorEmail");
 	let errorAddress = document.getElementById("errorAddress");
 	let errorPassword = document.getElementById("errorPassword");
-	let errorPhone = document.getElementById("errorPhone");
+	let errorPhone = document.getElementById("errorPhone"); */
+	document.getElementById('fName').classList.remove('is-invalid');
+	document.getElementById('fLastN').classList.remove('is-invalid');
+	document.getElementById('fEmail').classList.remove('is-invalid');
+	document.getElementById('fAddress').classList.remove('is-invalid');
+	document.getElementById('fPassword').classList.remove('is-invalid');
+	document.getElementById('fPhone').classList.remove('is-invalid');
+
 
 	// Validate fields entered by the user: name, phone, password, and email
 
@@ -28,41 +35,30 @@ function validate() {
 
 	if (fName.value == '' || fName.value.length <= 3 || !fName.value.match(letters)) {
 		document.getElementById('fName').classList.add('is-invalid');
-		error++;
+		
 	}
 	if (fEmail.value == '' || fEmail.length <= 3 || !fEmail.value.match(mailformat)) {
 		document.getElementById('fEmail').classList.add('is-invalid');
-		error++;
+		
 	}
 	if (fPhone.value == '' || fPhone.length <= 3 || !fPhone.value.match(numbers)) {
 		document.getElementById('fPhone').classList.add('is-invalid');
-		error++;
+		
 	}
 	if (fPassword.value == '' || fPassword.length <= 3 || !fPassword.value.match(letterNumber)) {
 		document.getElementById('fPassword').classList.add('is-invalid');
-		error++;
+		
 	}
 	if (fAddress.value == '' || fAddress.length <= 3) {
 		document.getElementById('fAddress').classList.add('is-invalid');
-		error++;
+	
 	}
 	if (fLastN.value == '' || fLastN.length <= 3) {
 		document.getElementById('fLastN').classList.add('is-invalid');
-		error++;
-	}
-
-	if (error > 0) {
-		return false;
-	} else {
-		return true;
+		
 	}
 	
-}
-function validation(){
+	event.preventDefault();
+    /* event.stopPropagation(); */
 
-if( fName.value ==='' || fEmail.value  ===''|| fAddress.value == '' ||fPassword.value == '' ||fPhone.value == '' ||fLastN.value == '' ){
-return false;
-}else{
-return true;
-}
 }
